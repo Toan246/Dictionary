181d8e523392393dda3a5ee94d68d24e8d0b6896
Toan246 <22024575@vnu.edu.vn>
2023-11-26 18:37:20 +0700

    2.0

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary();
-        DictionaryManagement management = new DictionaryManagement();
-        DictionaryCommandline commandline = new DictionaryCommandline();
-
-        commandline.dictionaryAdvanced();
-    }
-}
